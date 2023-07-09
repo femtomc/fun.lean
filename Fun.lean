15bba463e93088afe13eb7220e37c0d5f9b0fe67
McCoy Becker <mccoybecker@gmail.com>
2023-07-09 15:59:22 -0400

    Init.

--- Fun.lean
+++ Fun.lean
@@ -41,35 +41,44 @@ inductive HValue where -- HValue essentially stands for (higher-order abstract s
 -- Take an HValue which represents a Value, and return a Value.
 def unwrap (hv : HValue) : Except String Value :=
   match hv with
-    | HValue.HFunc _ => Except.error "`unwrap` called on `HFunc`, excepted `HVal"
+    | HValue.HFunc _ => Except.error "`unwrap` called on `HFunc`, expected `HVal"
     | HValue.HVal v => pure v
 
 inductive Environment α where
   | ENil : Environment α
   | ECons : Prod String α -> Environment α -> Environment α
+deriving Repr
 
 def empty_env : Environment HValue := Environment.ENil 
 
-def make_env (v : List (Prod String HValue)) : Environment HValue := match v with
-  | [] => empty_env
-  | α :: β => Environment.ECons α (make_env β)
+def make_env (v : List (Prod String HValue)) : Environment HValue := 
+  match v with
+    | [] => empty_env
+    | α :: β => Environment.ECons α (make_env β)
+
+def lifted_map (f : α -> Except String γ) (v : List α) : Except String (List γ) := 
+  match v with
+    | [] => pure []
+    | (x :: xs) => let lifted_cons := fun y: α => 
+      match (lifted_map f xs) with
+        | Except.ok u => pure (y :: u)
+        | Except.error e => Except.error e; 
+      (pure x) >>= lifted_cons
 
 -- (Except monad to propagate error)
-def apply (e : HValue) (args : List Value) : Except String HValue :=
+def apply (e : HValue) (args : List HValue) : Except String HValue :=
   match e with
     | HValue.HVal _ => Except.error "HVal provided to `apply`, expected `HFunc`"
-    | HValue.HFunc f => pure $ HValue.HVal (f args)
+    | HValue.HFunc f => HValue.HVal <$> (f <$> lifted_map unwrap args)
 
-def abstract (xs : List Value) (e : Expr) (env : Environment Value) : List Value -> Env := fun args => eval (defargs env xs args) e
-
-def eval (env : Environment Value) (expr : Expr) : Value :=
+def eval (env : Environment Value) (expr : Expr) : Except String HValue :=
   match expr with
-  | Expr.Number n => Value.VInt n
-  | Expr.Variable v => find env v
+  | Expr.Number n => pure $ HValue.HVal $ (Value.VInt n)
+  | Expr.Variable v => pure $ find env v
   | Expr.If e1 e2 e3 => match (eval env e1) with
-    | Value.VBool True => eval env e2
-    | Value.VBool False => eval env e3
-  | Expr.Apply f es => let ev := fun e1 => eval env e1; apply (eval f env) (map ev es)
+    | Value.VBool True => pure $ eval env e2
+    | Value.VBool False => pure $ eval env e3
+  | Expr.Apply f es => let ev := fun e1 => eval env e1; pure $ apply (eval f env) (map ev es)
   | Expr.Lambda xs e1 => abstract xs e1 env
 
 /-
